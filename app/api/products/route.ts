410b35c121ccbd8f2c5d769a9b4a909a8d0bd611
Mona Lisa <chutiphong.charoenchao@gmail.com>
2024-11-03 15:03:00 +0700

    🔧 FIx :: Fix Error when build

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,7 +2,7 @@ import { connectToDatabase } from "@/app/lib/mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: Request, res: NextApiResponse) {
   const db = await connectToDatabase();
 
   const url = new URL(req.url!);
@@ -10,28 +10,15 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
 
   const category = searchParams.get("category");
 
-  switch (req.method) {
-    case "GET":
-      if (category === "ทั้งหมด") {
-        const products = await db.collection("products").find().toArray();
-        return NextResponse.json(products);
-      } else {
-        const products = await db
-          .collection("products")
-          .find({ category })
-          .toArray();
-        return NextResponse.json(products);
-      }
+  if (category === "ทั้งหมด") {
+    const products = await db.collection("products").find().toArray();
+    return NextResponse.json(products);
+  } else {
+    const products = await db
+      .collection("products")
+      .find({ category })
+      .toArray();
 
-    case "POST":
-      return res.status(201).json({ message: "Add to cart" });
-
-    case "DELETE":
-      return res.status(200).json({ message: "Cart cleared" });
-
-    default:
-      return res
-        .status(405)
-        .json({ error: `Method ${req.method} not allowed` });
+    return NextResponse.json(products);
   }
 }
